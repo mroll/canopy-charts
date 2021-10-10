6f1f8a42063f9a0b92c6ff2dbcc63ae3cd214ba1
Matt Roll <mattroll@Matts-MacBook-Air.local>
2021-10-10 16:23:09 -0400

    add ability for text component to have dynamic values

--- src/DynamicText.tsx
+++ src/DynamicText.tsx
@@ -1,13 +1,80 @@
 import React from "react";
 import { useChartOps } from "./ChartOperations";
-import { ChartComponent } from "./types";
+import { ChartTable, ChartComponent, TableColumn } from "./types";
+import { getTableColumn } from "./util";
+
+interface Tag {
+  value: string;
+}
+
+type TagFunction = (table: ChartTable, arg?: string) => string;
+
+interface TagFunctionMap {
+  [key: string]: TagFunction;
+}
+
+const nthDatapointFn = (n: number) => {
+  return (table: ChartTable, columnName?: string) => {
+    const column = getTableColumn(table, columnName || "");
+    return "" + column.body[n >= 0 ? n : column.body.length + n];
+  };
+};
+
+const tagFunctionMap: TagFunctionMap = {
+  lastDatapoint: nthDatapointFn(-1),
+  secondToLastDatapoint: nthDatapointFn(-2),
+};
+
+function parseMixedTags(s: string) {
+  const data = [];
+  let i = 0;
+  const n = s.length;
+  while (i < n - 1) {
+    if (s[i] === "[" && s[i + 1] === "[") {
+      i += 2;
+      let objectString = "";
+      while (i < n && !(s[i] === "]" && s[i + 1] === "]")) {
+        objectString += s[i];
+        i++;
+      }
+
+      i += 2;
+      data.push(JSON.parse(objectString));
+    } else {
+      let currentString = "";
+      while (i < n && !(s[i] === "[" && s[i + 1] === "[")) {
+        currentString += s[i];
+        i++;
+      }
+
+      data.push(currentString);
+    }
+  }
+
+  return data;
+}
 
 function RenderDynamicText(props: any) {
   const { id, config, children } = props;
   const { font, display, margin, color, innerText } = config;
-  const { getComponents } = useChartOps();
+  const { getChartTable, getComponents } = useChartOps();
 
+  const chartTable = getChartTable();
   const childComponents = getComponents(children);
+  const mixedTags = parseMixedTags(innerText.trim());
+
+  const evalTag = (tag: Tag) => {
+    const funcallMatch = tag.value.match(/^(\w+)\(([\w\s]+)\)$/);
+    if (funcallMatch) {
+      const fn = tagFunctionMap[funcallMatch[1]];
+      const arg = funcallMatch[2];
+
+      return fn && fn(chartTable, arg);
+    }
+
+    const fn = tagFunctionMap[tag.value];
+    return fn && fn(chartTable);
+  };
 
   return (
     <div
@@ -35,7 +102,11 @@ function RenderDynamicText(props: any) {
               children={child.children}
             />
           ))
-        : innerText}
+        : mixedTags.map((tagComponent: string | Tag) =>
+            typeof tagComponent === "string"
+              ? tagComponent
+              : evalTag(tagComponent)
+          )}
     </div>
   );
 }
